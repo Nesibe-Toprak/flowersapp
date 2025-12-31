aca1a81c93865ba5ce54353f257ce04dbed986fc
Nesibe-Toprak <nt2007toprak@gmail.com>
2025-12-31 22:56:01 +0300

    Update app_constants.dart

--- flowersapp/lib/core/constants/app_constants.dart
+++ flowersapp/lib/core/constants/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   static const String supabaseUrl = 'https://unwntdjcgjbopaxfmhsc.supabase.co';
-  static const String supabaseAnonKey = 'sb_publishable_y8juZgLy2D8PTObxXxATDQ_SaGCFRzG';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVud250ZGpjZ2pib3BheGZtaHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMDEwMTMsImV4cCI6MjA4MjU3NzAxM30.EQoE4_iCNXkHl767ixLN240cjfVzOxQPRmpwG77ejoQ';
 }
