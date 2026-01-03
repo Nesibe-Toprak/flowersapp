fb129832316c5a013ff9908b661ee77175986501
Nesibe-Toprak <nt2007toprak@gmail.com>
2026-01-03 06:24:15 +0300

    Create breathing_widget.dart

---
+++ flowersapp/lib/presentation/widgets/breathing_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class BreathingWidget extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final double scaleFactor;
+
+  const BreathingWidget({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 2000),
+    this.scaleFactor = 0.05, // Scales between 1.0 and 1.05
+  });
+
+  @override
+  State<BreathingWidget> createState() => _BreathingWidgetState();
+}
+
+class _BreathingWidgetState extends State<BreathingWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(
+      begin: 1.0,
+      end: 1.0 + widget.scaleFactor,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _animation,
+      child: widget.child,
+    );
+  }
+}
